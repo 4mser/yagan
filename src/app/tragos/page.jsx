348de97b97318155064183f33e4ea995748a22f7
4mser <nicolas.moreno.sagan@gmail.com>
2024-01-23 15:54:33 -0300

    yagan

--- src/app/tragos/page.jsx
+++ src/app/tragos/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const page = () => {
   return (
     <div className='flex justify-center items-center'>
-        <img src="https://app-valdi.s3.amazonaws.com/carta.png" alt="carta tragos" className='w-full object-contain max-h-[93dvh]' />
+        <img src="https://app-valdi.s3.amazonaws.com/carta.png" alt="carta tragos" className='w-full object-contain max-h-[98dvh]' />
     </div>
   )
 }
