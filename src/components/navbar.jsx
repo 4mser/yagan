6dc09f8b754bf849e506be3c2001d9f685cd93f8
4mser <nicolas.moreno.sagan@gmail.com>
2024-01-23 15:42:59 -0300

    link navegacion

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import Image from 'next/image'
 import { usePathname } from 'next/navigation'
+import Link from 'next/link'
 
 
 const Navbar = () => {
@@ -9,24 +10,30 @@ const Navbar = () => {
 
   return (
     <nav className='fixed w-full bottom-0 left-0 flex justify-around items-center py-3 border-t border-white/10'>
-      <Image
-            src={`/icons/${pathname === '/carta' ? 'cartaActive.svg' : 'carta.svg'}`}
-            width={25}
-            height={25}
-            className={`${pathname === '/carta' ? 'drop-shadow-[0_0_0.3rem_#ffffff70]' : 'opacity-60'}`}
-        />
-        <Image
-            src={`/icons/${pathname === '/' ? 'homeActive.svg' : 'home.svg'}`}
-            width={25}
-            height={25}
-            className={`${pathname === '/' ? 'drop-shadow-[0_0_0.3rem_#ffffff70]' : 'opacity-60'}`}
-        />
+        <Link href={'/carta'} >
+          <Image
+              src={`/icons/${pathname === '/carta' ? 'cartaActive.svg' : 'carta.svg'}`}
+              width={25}
+              height={25}
+              className={`${pathname === '/carta' ? 'drop-shadow-[0_0_0.3rem_#ffffff70]' : 'opacity-60'}`}
+          />
+        </Link>
+        <Link href={'/'}>
+          <Image
+              src={`/icons/${pathname === '/' ? 'homeActive.svg' : 'home.svg'}`}
+              width={25}
+              height={25}
+              className={`${pathname === '/' ? 'drop-shadow-[0_0_0.3rem_#ffffff70]' : 'opacity-60'}`}
+          />
+        </Link>
+        <Link href={'/tragos'}>
         <Image
             src={`/icons/${pathname === '/tragos' ? 'cocktailActive.svg' : 'cocktail.svg'}`}
             width={25}
             height={25}
             className={`${pathname === '/tragos' ? 'drop-shadow-[0_0_0.3rem_#ffffff70]' : 'opacity-60'}`}
         />
+        </Link>
     </nav>
   )
 }
