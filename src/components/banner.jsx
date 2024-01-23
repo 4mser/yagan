fa674a8eed01cdfaac51eb803533fe4b0c28c8b3
4mser <nicolas.moreno.sagan@gmail.com>
2024-01-23 17:33:19 -0300

    navbar actualizado

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -4,10 +4,10 @@ import React from 'react'
 const Banner = () => {
   return (
     <Link href={'/carta'} >
-        <div className='fixed w-full top-14 h-10 left-0 bg-gradient-to-tr from-cyan-500 to-cyan-800 flex justify-center gap-2 px-4 py-2 items-center border-b border-white/10 '>
+        <div className='fixed w-full top-14 h-10 left-0 bg-gradient-to-tr from-red-600 to-red-900 flex justify-center gap-2 px-4 py-2 items-center border-b border-white/10 '>
 
-            <p>Explora el nuevo Menú</p>
-            <img src="/icons/shines.png" alt="" className='w-6' />
+            <p>Explora la nueva barra de Sushi</p>
+            <img src="/icons/sushi.svg" alt="" className='w-6' />
 
             </div>
     </Link>
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -9,30 +9,44 @@ const Navbar = () => {
   const pathname = usePathname()
 
   return (
-    <nav className='fixed z-50 bg-black/20 backdrop-blur-md w-full bottom-0 left-0 flex justify-around items-center py-3 border-t border-white/10'>
-        <Link href={'/carta'} >
-          <Image
-              src={`/icons/${pathname === '/carta' ? 'cartaActive.svg' : 'carta.svg'}`}
-              width={25}
-              height={25}
-              className={`${pathname === '/carta' ? '' : 'opacity-60'}`}
-          />
-        </Link>
-        <Link href={'/'}>
+    <nav className='fixed z-50 bg-black/20 backdrop-blur-md w-full bottom-0 left-0 flex  justify-around items-center py-3 border-t border-white/10'>
+      <Link href={'/'} className='flex h-10 justify-between items-center flex-col gap-[2px]'>
           <Image
               src={`/icons/${pathname === '/' ? 'homeActive.svg' : 'home.svg'}`}
               width={25}
               height={25}
               className={`${pathname === '/' ? '' : 'opacity-60'}`}
           />
+          <p className={`text-[9px] ${pathname === '/' ? '' : 'opacity-60'}`}>Inicio</p>
         </Link>
-        <Link href={'/tragos'}>
-        <Image
-            src={`/icons/${pathname === '/tragos' ? 'cocktailActive.svg' : 'cocktail.svg'}`}
-            width={25}
-            height={25}
-            className={`${pathname === '/tragos' ? '' : 'opacity-60'}`}
-        />
+        <Link href={'/tragos'} className='flex h-10 justify-between items-center flex-col gap-[2px]'>
+          <Image
+              src={`/icons/${pathname === '/tragos' ? 'cocktailActive.svg' : 'cocktail.svg'}`}
+              width={25}
+              height={25}
+              className={`${pathname === '/tragos' ? '' : 'opacity-60'} translate-x-[3px]`}
+          />
+          <p className={`text-[9px] ${pathname === '/tragos' ? '' : 'opacity-60'}`}>Tragos</p>
+        </Link>
+        <Link href={'/carta'} className='flex h-10 justify-between items-center flex-col gap-[2px]' >
+          <Image
+              src={`/icons/${pathname === '/carta' ? 'cartaActive.svg' : 'carta.svg'}`}
+              width={21}
+              height={21}
+              className={`${pathname === '/carta' ? '' : 'opacity-60'} translate-y-[3px]`}
+          />
+          <p className={`text-[9px] ${pathname === '/carta' ? '' : 'opacity-60'}`}>Comida</p>
+        </Link>
+        
+        
+        <Link href={'/sushi'} className='flex h-10 justify-between items-center flex-col gap-[2px]' >
+          <Image
+              src={`/icons/${pathname === '/sushi' ? 'sushiActive.svg' : 'sushimenu.svg'}`}
+              width={26}
+              height={26}
+              className={`${pathname === '/sushi' ? '' : 'opacity-60'} translate-x-[2px] translate-y-[3px]`}
+          />
+          <p className={`text-[9px] ${pathname === '/sushi' ? '' : 'opacity-60'}`}>Sushi</p>
         </Link>
     </nav>
   )
