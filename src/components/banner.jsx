3f6c521db2d78d30d8b29cf83162bc7a5bc56596
4mser <nicolas.moreno.sagan@gmail.com>
2024-01-23 17:45:35 -0300

    banner expandido

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Banner = () => {
   return (
     <Link href={'/carta'} >
-        <div className='fixed w-full top-14 h-10 left-0 bg-gradient-to-tr from-red-600 to-red-900 flex justify-center gap-2 px-4 py-2 items-center border-b border-white/10 '>
+        <div className='fixed w-full top-14 h-12 left-0 bg-gradient-to-tr from-red-600 to-red-900 flex justify-center gap-2 px-4 py-2 items-center border-b border-white/10 '>
 
             <p>Explora la nueva barra de Sushi</p>
             <img src="/icons/sushi.svg" alt="" className='w-6' />
