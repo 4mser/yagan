60b9cdb835ce453953593118866606c94207ce49
4mser <nicolas.moreno.sagan@gmail.com>
2024-01-25 19:13:39 -0300

    3

--- src/components/contacto.jsx
+++ src/components/contacto.jsx
@@ -4,7 +4,7 @@ const Contacto = () => {
   return (
     <section className='fixed bottom-4 right-4'>
         <div className='rounded-full bg-gradient-to-tr to-pink-600/70 from-red-800/70  w-14 h-14 flex justify-center items-center'>
-            <img src="/icons/pushpin.svg" alt="" className='w-6' />
+            <img src="/icons/pushpin.svg" alt="" className='w-7' />
         </div>
     </section>
   )
