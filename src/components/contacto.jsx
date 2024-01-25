92b9c74f1390c30b35c0a06065bbf3f6c8502e72
4mser <nicolas.moreno.sagan@gmail.com>
2024-01-25 20:34:24 -0300

    cambio color logo

--- src/components/contacto.jsx
+++ src/components/contacto.jsx
@@ -27,39 +27,39 @@ const Contacto = () => {
 
   return (
     <section className='fixed bottom-4 right-4 z-40'>
-      <motion.div
+      <motion.button
         className='rounded-full bg-gradient-to-tr from-slate-500 to-slate-800 w-14 h-14 flex justify-center items-center'
         onClick={toggleMenu}
         variants={buttonVariants}
         animate={menuOpen ? "open" : "closed"}
       >
         <img src="/icons/pushpin.svg" alt="" className='w-7' />
-      </motion.div>
+      </motion.button>
 
-      <motion.div
+      <motion.button
         className='absolute bottom-0 right-16 rounded-full bg-gradient-to-tr from-pink-500 via-red-500 to-yellow-500 w-10 h-10 flex justify-center items-center'
         variants={menuVariants}
         animate={menuOpen ? "open" : "closed"}
         onClick={goToInstagram}
       >
         <img src="/icons/instagram.svg" alt="" className='w-6 opacity-85' />
-      </motion.div>
+      </motion.button>
 
-      <motion.div
+      <motion.button
         className='absolute bottom-16 right-0 rounded-full bg-gradient-to-tr from-blue-500 to-blue-900 w-10 h-10 flex justify-center items-center'
         variants={menuVariants}
         animate={menuOpen ? "open" : "closed"}
       >
         <img src="/icons/mail.svg" alt="" className='w-6 opacity-85' />
-      </motion.div>
+      </motion.button>
 
-      <motion.div
+      <motion.button
         className='absolute bottom-12 right-12 rounded-full bg-gradient-to-tr from-green-600 to-yellow-500 w-10 h-10 flex justify-center items-center'
         variants={menuVariants}
         animate={menuOpen ? "open" : "closed"}
       >
         <img src="/icons/wsp.svg" alt="" className='w-6 opacity-85' />
-      </motion.div>
+      </motion.button>
     </section>
   )
 }
--- src/components/logo.jsx
+++ src/components/logo.jsx
@@ -13,7 +13,7 @@ const Logo = () => {
             src={'/images/logo-rojo.png'}
             width={130}
             height={20}
-            className={`-translate-y-1 ${pathname === '/tragos' ? '-hue-rotate-[170deg] saturate-150 brightness-150' : ''}`}
+            className={`-translate-y-1 ${pathname === '/tragos' ? '-hue-rotate-[170deg] saturate-150 brightness-150' : ''} ${pathname === '/carta' ? 'hue-rotate-[63deg] brightness-[2] saturate-[3]' : ''}`}
         />
     </Link>
   )
