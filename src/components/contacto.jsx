6b6f8b80fad0cda71e640b0a031272d3031cc101
4mser <nicolas.moreno.sagan@gmail.com>
2024-01-25 19:32:40 -0300

    animacion boton

--- src/components/contacto.jsx
+++ src/components/contacto.jsx
@@ -9,21 +9,32 @@ const Contacto = () => {
     setMenuOpen(!menuOpen);
   };
 
-  // Define las variantes para las animaciones
-  const variants = {
+  // Variantes para los íconos del menú
+  const menuVariants = {
     open: { opacity: 1, scale: 1, transition: { type: 'spring', stiffness: 260, damping: 20 } },
     closed: { opacity: 0, scale: 0 }
   };
 
+  // Variantes para el primer div (botón principal)
+  const buttonVariants = {
+    open: { rotate: 15, scale: 1.1 },
+    closed: { rotate: 0, scale: 1 }
+  };
+
   return (
     <section className='fixed bottom-4 right-4'>
-      <div className='rounded-full bg-gradient-to-tr from-slate-500 to-slate-800 w-14 h-14 flex justify-center items-center' onClick={toggleMenu}>
+      <motion.div
+        className='rounded-full bg-gradient-to-tr from-slate-500 to-slate-800 w-14 h-14 flex justify-center items-center'
+        onClick={toggleMenu}
+        variants={buttonVariants}
+        animate={menuOpen ? "open" : "closed"}
+      >
         <img src="/icons/pushpin.svg" alt="" className='w-7' />
-      </div>
+      </motion.div>
 
       <motion.div
         className='absolute bottom-0 right-16 rounded-full bg-gradient-to-tr from-pink-500 via-red-500 to-yellow-500 w-10 h-10 flex justify-center items-center'
-        variants={variants}
+        variants={menuVariants}
         animate={menuOpen ? "open" : "closed"}
       >
         <img src="/icons/instagram.svg" alt="" className='w-6 opacity-85' />
@@ -31,7 +42,7 @@ const Contacto = () => {
 
       <motion.div
         className='absolute bottom-16 right-0 rounded-full bg-gradient-to-tr from-red-500 to-red-900 w-10 h-10 flex justify-center items-center'
-        variants={variants}
+        variants={menuVariants}
         animate={menuOpen ? "open" : "closed"}
       >
         <img src="/icons/mail.svg" alt="" className='w-6 opacity-85' />
@@ -39,7 +50,7 @@ const Contacto = () => {
 
       <motion.div
         className='absolute bottom-12 right-12 rounded-full bg-gradient-to-tr from-green-600 to-yellow-500 w-10 h-10 flex justify-center items-center'
-        variants={variants}
+        variants={menuVariants}
         animate={menuOpen ? "open" : "closed"}
       >
         <img src="/icons/wsp.svg" alt="" className='w-6 opacity-85' />
