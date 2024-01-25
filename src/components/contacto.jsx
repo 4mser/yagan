84346d4d8d0fb9106b0eb3683c4d1025b831bb2d
4mser <nicolas.moreno.sagan@gmail.com>
2024-01-25 19:11:41 -0300

    2

--- src/components/contacto.jsx
+++ src/components/contacto.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Contacto = () => {
   return (
     <section className='fixed bottom-4 right-4'>
-        <div className='rounded-full bg-gradient-to-tr to-pink-600/70 from-red-800/70  w-12 h-12 flex justify-center items-center'>
+        <div className='rounded-full bg-gradient-to-tr to-pink-600/70 from-red-800/70  w-14 h-14 flex justify-center items-center'>
             <img src="/icons/pushpin.svg" alt="" className='w-6' />
         </div>
     </section>
