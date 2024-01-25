801b1347a0ccb427e82707eac51dc62cfddd959a
4mser <nicolas.moreno.sagan@gmail.com>
2024-01-24 21:41:59 -0300

    metadata image

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -13,6 +13,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta property="og:image" content="https://app-valdi.s3.amazonaws.com/yagan/logotipo_Mesa+de+trabajo+1+copia+5.png" />
+      </head>
       
       <body>
         <Topbar />
