8ad51e404e8b2358269dde08b26a4e899d28069e
4mser <nicolas.moreno.sagan@gmail.com>
2024-02-27 17:53:22 -0300

    nuevos rolls

--- src/app/sushi/page.jsx
+++ src/app/sushi/page.jsx
@@ -1,9 +1,163 @@
-import React from 'react'
+import React from 'react';
 
-const page = () => {
+const Page = () => {
+
+  const gohans = [
+    {
+      name: "GOHAN SHIRASHI EBI",
+      description: "Arroz de sushi, cebollín, palta, queso crema, sésamo y camarón.",
+      price: "9.990"
+    },
+    {
+      name: "SHIRASHI EBI TERI",
+      description: "Arroz de sushi, palta, queso crema, cebollín, sésamo, pollo teriyaki y camarón.",
+      price: "10.990"
+    },
+    {
+      name: "SHIRASHI PULPO",
+      description: "Arroz de sushi, pulpo, queso crema, palmito, palta y cebollín.",
+      price: "12.990"
+    },
+    {
+      name: "SHIRASHI SAKE",
+      description: "Arroz de sushi, palta, queso crema, cebollín, sésamo y salmón.",
+      price: "9.990"
+    },
+    {
+      name: "SHIRASHI SUPER EXPERTO",
+      description: "Arroz de sushi, Atún, masago, palta, salmón, camarón, wasabi y jengibre.",
+      price: "14.990"
+    },
+    {
+      name: "SHIRASHI TERI",
+      description: "Arroz de sushi, cebollín, palta, sésamo, queso crema y pollo teriyaki.",
+      price: "8.990"
+    },
+  ]
+
+  const rollspecials = [
+    {
+      name: "ROLLS ACEVICHADO (70)",
+      description: "Camarón Apanado, Queso Crema, Bañado en Salsa Acevichada y Coronado con Maní Crocante.",
+      price: "6.900"
+    },
+    {
+      name: "PREMIUM YAGAN (72)",
+      description: "Salmón Apanado, Palta, Envuelto en Queso Crema Flameado Coronado En Salsa Teriyaki y Papas Hilo.",
+      price: "7.900"
+    },{
+      name: "ROLLS CEVICHE (76)",
+      description: "Salmón, Queso Crema, Cebollín envuelto en Palta Bañado en Ceviche de la Casa.",
+      price: "8.500"
+    },{
+      name: "RES ROLLS (78)",
+      description: "Filete de Res, Queso Mantecoso, apanado en panko bañado con salsa acevichada y tiritas de wantan.",
+      price: "7.800"
+    },{
+      name: "EBI ACEVICHADO (79)",
+      description: "Salmón Apanado, Queso Crema envuelto en Palta y Camarón Acevichado.",
+      price: "7.900"
+    },{
+      name: "MEXICAN ROLLS (80)",
+      description: "Pollo Apanado, ciboullete, queso crema envuelto en sesamo coronado con Guacamole.",
+      price: "7.900"
+    },
+  ]
+
+  const envueltospalta = [
+    {
+      name: "EBI TERIYAKI (1)",
+      description: "Camarón, Cebollín, Queso Crema envuelto en Palta Bañado en Pollo Teriyaky y Sésamo",
+      price: "7.200"
+    },
+    {
+      name: "PREMIUM (3)",
+      description: "Salmón, Ciboullete, Queso Crema envuelto en Palta.",
+      price: "6.900"
+    },
+    {
+      name: "EBI CHEESE (4)",
+      description: "Camarón, Cebollín, Queso Crema envuelto en Palta.",
+      price: "6.900"
+    },
+    {
+      name: "CHICKEN LIGHT (5)",
+      description: "Pollo, Cebollín, Queso Crema envuelto en Palta.",
+      price: "6.800"
+    },
+    {
+      name: "EBI TEMPURA (6)",
+      description: "Camarón Tempura, Queso Crema envuelto en Palta.",
+      price: "6.900"
+    },
+    {
+      name: "OKAMA ROLLS (10)",
+      description: "Pollo, Queso Crema, Nuez envuelto en Palta Bañado en Salsa Acevichada.",
+      price: "6.900"
+    },
+  ]
   return (
-    <div>sushi</div>
-  )
+    <div className='pt-14'>
+      {/* CLÁSICOS Section */}
+      <div>
+      <h2 className='text-lg font-semibold bg-gradient-to-tr from-red-700 to-red-950  flex items-center px-4 py-3'>SUSHI ROLL SPECIAL {'(9 PIEZAS)'}</h2>
+          {rollspecials.map((rolls, index) => (
+            <li key={index} className='flex justify-between items-center px-4 py-1 mt-3 gap-10'>
+              <div className=''>
+                <h1 className='font-semibold text-xs'>
+                  {rolls.name}
+                </h1>
+                <p className='font-normal opacity-80 text-xs'>
+                  {rolls.description}
+                </p>
+              </div>
+              <div className='text-sm w-20 text-center font-medium border text-red-500 border-red-500 px-2 rounded-lg'>
+                ${rolls.price}
+              </div>
+            </li>
+          ))}
+        <br />
+        <h2 className='text-lg font-semibold bg-gradient-to-tr from-red-950 to-amber-500  flex items-center px-4 py-3'>GOHAN</h2>
+        <ul className='text-xs  flex  flex-col'>
+          {gohans.map((gohan, index) => (
+            <li key={index} className='flex justify-between items-center px-4 py-1 mt-3 gap-8'>
+              <div className=''>
+                <h1 className='font-semibold'>
+                  {gohan.name}
+                </h1>
+                <p className='font-normal opacity-80'>
+                  {gohan.description}
+                </p>
+              </div>
+                <span className='text-sm w-20 text-center font-medium border text-orange-400 border-orange-400 px-2 rounded-lg'>
+                  ${gohan.price}
+                </span>
+            </li>
+          ))}
+          <br />
+          <h2 className='text-lg font-semibold bg-gradient-to-tr from-green-700 to-yellow-600  flex items-center px-4 py-3'>SUSHI ROLL SPECIAL {'(9 PIEZAS)'}</h2>
+          {envueltospalta.map((rolls, index) => (
+            <li key={index} className='flex justify-between items-center px-4 py-1 mt-3 gap-10'>
+              <div className=''>
+                <h1 className='font-semibold text-xs'>
+                  {rolls.name}
+                </h1>
+                <p className='font-normal opacity-80 text-xs'>
+                  {rolls.description}
+                </p>
+              </div>
+              <div className='text-sm w-20 text-center font-medium border text-lime-400 border-lime-400 px-2 rounded-lg'>
+                ${rolls.price}
+              </div>
+            </li>
+          ))}
+          <br />
+        </ul>
+      </div>
+
+      
+    </div>
+  );
 }
 
-export default page
+export default Page;
--- src/app/tragos/page.jsx
+++ src/app/tragos/page.jsx
@@ -1,114 +1,91 @@
 import React from 'react';
 
 const Page = () => {
+
+  const tragos = [
+    {
+      name: "CERVEZA KROSS 470 CM3",
+      description: "",
+      price: "3.490"
+    },
+    {
+      name: "VINO BOTELLA",
+      description: "",
+      price: "15.000"
+    },
+    {
+      name: "GIN",
+      description: "",
+      price: "7.000"
+    },
+    {
+      name: "RAMAZZOTI",
+      description: "",
+      price: "7.000"
+    },
+    {
+      name: "PISCO SOUR CATEDRAL",
+      description: "",
+      price: "4.990"
+    },
+    {
+      name: "JACK DANIELS GENTLEMAN",
+      description: "",
+      price: "8.990"
+    },
+    {
+      name: "MOJITO",
+      description: "",
+      price: "4.600"
+    },
+    {
+      name: "JACK DANIELS",
+      description: "",
+      price: "7.490"
+    },
+    {
+      name: "MOJITO SABORES",
+      description: "",
+      price: "5.590"
+    },
+    {
+      name: "PISCOLA",
+      description: "",
+      price: "6.000"
+    },
+    {
+      name: "PISCOLA 2X",
+      description: "",
+      price: "9.990"
+    }
+  ]
   return (
     <div className='pt-14'>
       {/* CLÁSICOS Section */}
       <div>
-        <h2 className='text-lg font-semibold bg-gradient-to-tr from-cyan-600 to-blue-950 flex items-center px-4 py-3'>CLÁSICOS</h2>
-        <ul className='text-xs p-4'>
-          <li>
-            <div className='flex items-center justify-between'>
-              <strong>Piscola</strong><span className='font-bold text-cyan-500'>$8</span>
-            </div>
-            <p className='font-normal opacity-80'>Pisco, Coca Cola.</p>
-          </li>
+        <h2 className='text-lg font-semibold bg-gradient-to-tr to-cyan-500 from-blue-950 flex items-center px-4 py-3'>TRAGOS</h2>
+        <ul className='text-xs p-4 flex gap-3 flex-col'>
+          {tragos.map((trago, index) => (
+            <li key={index}>
+              <div className='flex items-center justify-between'>
+                <h1 className='font-semibold'>
+                  {trago.name}
+                </h1>
+                <span className='text-sm w-20 text-center font-medium border text-cyan-400 border-cyan-400 px-2 rounded-lg'>
+                  ${trago.price}
+                </span>
+              </div>
+              <p className='font-normal opacity-80'>
+                {trago.description}
+              </p>
+            </li>
+          ))}
           <br />
-          <li>
-            <div className='flex items-center justify-between'>
-              <strong>Pisco Sour</strong><span className='font-bold text-cyan-500'>$9</span>
-            </div>
-            <p className='font-normal opacity-80'>Limón, pisco, goma, albumina, amargo de Angostura.</p>
-          </li>
+          
         </ul>
       </div>
 
-      {/* MOJITOS Section */}
-      <div>
-        <h2 className='text-lg font-semibold bg-gradient-to-tr from-green-600 to-blue-300 flex items-center px-4 py-3'>MOJITOS</h2>
-        <ul className='text-xs p-4'>
-          <li>
-            <div className='flex items-center justify-between'>
-              <strong>Mojito Tradicional</strong><span className='font-bold text-green-300'>$10</span>
-            </div>
-            <p className='font-normal opacity-80'>Menta, limones, ron blanco, limón, goma, agua mineral.</p>
-          </li>
-          <br />
-          <li>
-            <div className='flex items-center justify-between'>
-              <strong>Mojito Ramazzotti</strong><span className='font-bold text-green-300'>$11</span>
-            </div>
-            <p className='font-normal opacity-80'>Menta, naranja, limón, ron blanco, jugo de naranja, Ramazzotti, agua mineral.</p>
-          </li>
-        </ul>
-      </div>
-
-      {/* AUTOR Section */}
-      <div>
-        <h2 className='text-lg font-semibold bg-gradient-to-tr from-purple-500 to-pink-500 flex items-center px-4 py-3'>AUTOR</h2>
-        <ul className='text-xs p-4'>
-          <li>
-            <div className='flex items-center justify-between'>
-              <strong>Cazador Furtivo</strong><span className='font-bold text-violet-300'>$12</span>
-            </div>
-            <p className='font-normal opacity-80'>Jägermeister, triple sec, limón, jugo de naranja.</p>
-          </li>
-          <br />
-          <li>
-            <div className='flex items-center justify-between'>
-              <strong>Amore Milano</strong><span className='font-bold text-violet-300'>$13</span>
-            </div>
-            <p className='font-normal opacity-80'>Whisky, Campari, jugo de pomelo, jugo de limón, goma.</p>
-          </li>
-        </ul>
-      </div>
-
-      {/* BAR SIN ALCOHOL Section */}
-      <div>
-        <h2 className='text-lg font-semibold bg-gradient-to-tr from-slate-500 to-slate-800 flex items-center px-4 py-3'>BAR SIN ALCOHOL</h2>
-        <ul className='text-xs p-4'>
-          <li>
-            <div className='flex items-center justify-between'>
-              <strong>Jugos del Día</strong><span className='font-bold text-slate-400'>$6</span>
-            </div>
-            <p className='font-normal opacity-80'>Variedad de jugos frescos disponibles diariamente.</p>
-          </li>
-          <br />
-          <li>
-            <div className='flex items-center justify-between'>
-              <strong>Cordillerano Autor</strong><span className='font-bold text-slate-400'>$7</span>
-            </div>
-            <p className='font-normal opacity-80'>Amargo de Angostura, Sprite, azahar, soda, limón.</p>
-          </li>
-        </ul>
-      </div>
-
-      {/* MICHELADAS Section */}
-      <div>
-        <h2 className='text-lg font-semibold bg-gradient-to-tr from-yellow-400 to-orange-400 flex items-center px-4 py-3'>MICHELADAS</h2>
-        <ul className='text-xs p-4'>
-          <li>
-            <div className='flex items-center justify-between'>
-              <strong>Chelada Sal de Autor</strong><span className='font-bold text-yellow-300'>$5</span>
-            </div>
-            <p className='font-normal opacity-80'>Sal de autor.</p>
-          </li>
-          <br />
-          <li>
-            <div className='flex items-center justify-between'>
-              <strong>Michelada</strong><span className='font-bold text-yellow-300'>$5</span>
-            </div>
-            <p className='font-normal opacity-80'>Tabasco, merken, sal de autor, salsa inglesa.</p>
-          </li>
-          <br />
-          <li>
-            <div className='flex items-center justify-between'>
-              <strong>Michelada Bloody</strong><span className='font-bold text-yellow-300'>$6</span>
-            </div>
-            <p className='font-normal opacity-80'>Todo lo anterior más bloody de tomate.</p>
-          </li>
-        </ul>
-      </div>
+      
     </div>
   );
 }
